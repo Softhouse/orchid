54e6e2a11ed845f8f44e55482a141bc4b99511ef
svahn <mikael.k.svahn@gmail.com>
2012-09-13 21:21:07 +0200

    Added javadoc

--- se.softhouse.garden.orchid.vaadin/addon/src/main/java/se/softhouse/garden/orchid/vaadin/OrchidNumberField.java
+++ se.softhouse.garden.orchid.vaadin/addon/src/main/java/se/softhouse/garden/orchid/vaadin/OrchidNumberField.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2012, Mikael Svahn, Softhouse Consulting AB
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so:
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package se.softhouse.garden.orchid.vaadin;
 
 import se.softhouse.garden.orchid.vaadin.widgetset.client.ui.VOrchidNumberField;
@@ -6,11 +24,21 @@ import com.vaadin.terminal.PaintException;
 import com.vaadin.terminal.PaintTarget;
 import com.vaadin.ui.ClientWidget;
 import com.vaadin.ui.ClientWidget.LoadStyle;
-import com.vaadin.ui.TextField;
 
+import de.essendi.vaadin.ui.component.numberfield.NumberField;
+
+/**
+ * This is an html5 extension of the NumberField addon. It enables to set the
+ * type, pattern and step attributes. Provides a numeric field with automatic
+ * keystroke filtering and validation for integer (123) and decimal numbers
+ * (12.3). The minus sign and user-definable grouping and decimal separators are
+ * supported.
+ * 
+ * @author Mikael Svahn
+ */
 @SuppressWarnings("serial")
 @ClientWidget(value = VOrchidNumberField.class, loadStyle = LoadStyle.EAGER)
-public class OrchidNumberField extends TextField {
+public class OrchidNumberField extends NumberField {
 
 	private String inputType = "text";
 	private String inputPattern = null;
@@ -31,26 +59,53 @@ public class OrchidNumberField extends TextField {
 		}
 	}
 
+	/**
+	 * Return the type attribute of the input tag
+	 */
 	public String getInputType() {
 		return this.inputType;
 	}
 
+	/**
+	 * Set the type attribute of the input tag.
+	 * 
+	 * @param type
+	 *            As defined in html
+	 */
 	public void setInputType(String type) {
 		this.inputType = type;
 	}
 
+	/**
+	 * Return the pattern attribute of the input tag
+	 */
 	public String getInputPattern() {
 		return this.inputPattern;
 	}
 
+	/**
+	 * Set the pattern attribute of the input tag.
+	 * 
+	 * @param pattern
+	 *            As defined in html
+	 */
 	public void setInputPattern(String pattern) {
 		this.inputPattern = pattern;
 	}
 
+	/**
+	 * Return the step attribute of the input tag
+	 */
 	public String getInputStep() {
 		return this.inputStep;
 	}
 
+	/**
+	 * Set the step attribute of the input tag.
+	 * 
+	 * @param step
+	 *            As defined in html
+	 */
 	public void setInputStep(String step) {
 		this.inputStep = step;
 	}
