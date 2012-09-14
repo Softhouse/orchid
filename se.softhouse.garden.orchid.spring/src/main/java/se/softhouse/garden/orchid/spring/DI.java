4c5158c3de6d2f449c9546f00e54baa030029803
svahn <mikael.k.svahn@gmail.com>
2012-09-14 22:24:13 +0200

    Added javadoc

--- se.softhouse.garden.orchid.spring/src/main/java/se/softhouse/garden/orchid/spring/DI.java
+++ se.softhouse.garden.orchid.spring/src/main/java/se/softhouse/garden/orchid/spring/DI.java
@@ -1,11 +1,56 @@
+/**
+ * Copyright (c) 2012, Mikael Svahn, Softhouse Consulting AB
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so:
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package se.softhouse.garden.orchid.spring;
 
+/**
+ * Provids methods to inject spring beans into an object. This is an altenative
+ * to use the @Configurable annotation. To use it add following declaration to
+ * the class
+ * 
+ * <pre>
+ * &#064;SuppressWarnings(&quot;unused&quot;)
+ * private final Void di = DI.inject(this);
+ * </pre>
+ * 
+ * @author Mikael Svahn
+ * 
+ */
 public class DI {
+	/**
+	 * Inject all beans annotated with @Resource or @Autowired
+	 * 
+	 * @param obj
+	 *            The object to configure
+	 * @return Void
+	 */
 	public static Void inject(Object obj) {
 		SpringBeanConfigurer.inject(obj);
 		return null;
 	}
 
+	/**
+	 * Lookup a bean in the SpringContext
+	 * 
+	 * @param clazz
+	 *            The class of the bean to lookup
+	 * @return The found bean
+	 */
 	public static <T> T bean(Class<T> clazz) {
 		return SpringBeanConfigurer.bean(clazz);
 	}
--- se.softhouse.garden.orchid.spring/src/main/java/se/softhouse/garden/orchid/spring/SpringBeanConfigurer.java
+++ se.softhouse.garden.orchid.spring/src/main/java/se/softhouse/garden/orchid/spring/SpringBeanConfigurer.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2012, Mikael Svahn, Softhouse Consulting AB
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so:
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package se.softhouse.garden.orchid.spring;
 
 import org.springframework.beans.factory.wiring.BeanConfigurerSupport;
@@ -5,6 +23,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
+/**
+ * Used by the DI class to lookup beans in the SpringContext This bean must be
+ * instanciated in the applicationContext.
+ * 
+ * @author Mikael Svahn
+ */
 public class SpringBeanConfigurer implements ApplicationListener<ContextRefreshedEvent> {
 
 	private static ApplicationContext context = null;
